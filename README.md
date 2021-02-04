3de39b112c5cefeaec708ed4e8bdcf77e6504eda
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2021-02-04 17:37:00 +0700

    README.md

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MyLibNFCReaderExample
- 
+From library https://github.com/gzeinnumer/MyLibNFCReader
+
+[Download Sample App](https://drive.google.com/file/d/1FHteEi9dZt6bVAqN19TiYOfeZ9N2OB0_/view?usp=sharing)
+
+---
+
+```
+Copyright 2021 M. Fadli Zein
+```
--- app/src/main/java/com/gzeinnumer/mylibnfcreaderexample/MainActivity.java
+++ app/src/main/java/com/gzeinnumer/mylibnfcreaderexample/MainActivity.java
@@ -1,8 +1,17 @@
 package com.gzeinnumer.mylibnfcreaderexample;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.gzeinnumer.mylibnfcreader.helper.EndNFCTagging;
+import com.gzeinnumer.mylibnfcreader.helper.NFCCallBack;
+import com.gzeinnumer.mylibnfcreader.helper.NFCErrorCallBack;
+import com.gzeinnumer.mylibnfcreader.helper.NFCTools;
+import com.gzeinnumer.mylibnfcreader.lib.StartNFCTagging;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,47 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_run_nfc_type_1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (NFCTools.checkNFCHardware(MainActivity.this)) {
+                    Toast.makeText(getApplicationContext(), "NFC Support", Toast.LENGTH_SHORT).show();
+                    new StartNFCTagging(MainActivity.this);
+                } else {
+                    Toast.makeText(getApplicationContext(), "NFC Not Support", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        findViewById(R.id.btn_run_nfc_type_2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new StartNFCTagging(MainActivity.this, new NFCErrorCallBack() {
+                    @Override
+                    public void onNotSupport(String msg) {
+                        //if Device not Support NFC
+                        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        new EndNFCTagging(intent, getSupportFragmentManager()).observer(new NFCCallBack() {
+            @Override
+            public void callBack(String idHex, String idReversedHex, String idDec, String idReversedDec) {
+                String str = "";
+                str = str + "onNewIntent: Hexadecimal Code          " + idHex;
+                str = str + "onNewIntent: Reversed Hexadecimal Code " + idReversedHex;
+                str = str + "onNewIntent: Decimal Code              " + idDec;
+                str = str + "onNewIntent: Reversed Decimal Code     " + idReversedDec;
+
+                Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
